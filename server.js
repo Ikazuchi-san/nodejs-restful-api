202af84fd88944afdd38b2eb1adb361f0787d721
sparkstorm44 <paulo.tarso.nr77@gmail.com>
2021-03-24 19:31:53 -0300

    Added working API that provides client data on /clients

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require ('express');
 const app = express();
+const data = require('./data-example.json');
+
+// for testing API client => https://jsonplaceholder.typicode.com/users
+
+app.use(express.json());
+
+app.get("/clients", function(req, res) {
+    res.json(data);
+});
+app.get("/clients/:id", function(req, res) {});
+app.post("/clients", function(req, res) {});
+app.put("/clients", function(req, res) {});
+app.put("/clients:id", function(req, res) {});
+app.delete("/clients", function(req, res) {});
+app.delete("/clients:id", function(req, res) {});
 
 app.listen(5000, function(){
     console.log("Server is running");
